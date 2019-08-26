a9530211c8f6150c7ce56925f457034d5e16b9f3
Henning Håkonsen <henninghaakonsen@me.com>
2019-08-26 20:57:24 +0200

    Kaller api med data fra deltaker.

--- src/App.jsx
+++ src/App.jsx
@@ -1,281 +1,328 @@
-import React, { useState, useEffect } from 'react';
-import AceEditor from 'react-ace';
+import React, { useState, useEffect } from "react";
+import AceEditor from "react-ace";
+import axios from "axios";
+import { debounce } from "lodash";
 
-import 'brace/mode/html';
-import 'brace/theme/vibrant_ink';
-import 'brace/ext/searchbox';
-import 'brace/ext/language_tools';
-import Instructions from './Instructions';
+import "brace/mode/html";
+import "brace/theme/vibrant_ink";
+import "brace/ext/searchbox";
+import "brace/ext/language_tools";
+import Instructions from "./Instructions";
 
 import {
-  EXCLAMATIONS,
-  POWER_MODE_ACTIVATION_THRESHOLD,
-  PARTICLE_ALPHA_FADEOUT,
-  PARTICLE_COLORS,
-  PARTICLE_GRAVITY,
-  PARTICLE_NUM_RANGE,
-  PARTICLE_SIZE,
-  PARTICLE_VELOCITY_RANGE,
-  MAX_PARTICLES
-} from './constants';
+    EXCLAMATIONS,
+    MAX_PARTICLES,
+    PARTICLE_ALPHA_FADEOUT,
+    PARTICLE_COLORS,
+    PARTICLE_GRAVITY,
+    PARTICLE_NUM_RANGE,
+    PARTICLE_SIZE,
+    PARTICLE_VELOCITY_RANGE,
+    POWER_MODE_ACTIVATION_THRESHOLD
+} from "./constants";
 
 let streakTimeout, saveContentTimeout;
 
 const sample = arr => {
-  const len = arr == null ? 0 : arr.length;
-  return len ? arr[Math.floor(Math.random() * len)] : undefined;
+    const len = arr == null ? 0 : arr.length;
+    return len ? arr[Math.floor(Math.random() * len)] : undefined;
 };
 
 let particles = [];
 let particlePointer = 0;
 
 const App = () => {
-  const [streak, updateStreak] = useState(0);
-  const [animate, setAnimate] = useState(false);
-  const [content, setContent] = useState(localStorage.getItem('content') || '');
-  const [animationKey, setAnimationKey] = useState(0);
-  const [name, setName] = useState('Unnamed');
-  const [exclamation, setExclamation] = useState(undefined);
-  const [viewInstructions, setViewInstructions] = useState(false);
-  const [powerMode, setPowerMode] = useState(false);
-  const [editor, setEditor] = useState(undefined);
-  const [lastDraw, setLastDraw] = useState(0);
-  const [ctx, setCtx] = useState(undefined);
-  const [inputType, setInputType] = useState(undefined);
+    const [streak, updateStreak] = useState(0);
+    const [animate, setAnimate] = useState(false);
+    const [content, setContent] = useState(
+        localStorage.getItem("content") || ""
+    );
+    const [animationKey, setAnimationKey] = useState(0);
+    const [name, setName] = useState("Unnamed");
+    const [exclamation, setExclamation] = useState(undefined);
+    const [viewInstructions, setViewInstructions] = useState(false);
+    const [powerMode, setPowerMode] = useState(false);
+    const [editor, setEditor] = useState(undefined);
+    const [lastDraw, setLastDraw] = useState(0);
+    const [ctx, setCtx] = useState(undefined);
+    const [inputType, setInputType] = useState(undefined);
 
-  const onChange = (value, data) => {
-    const insertTextAction = data.action === 'insert';
+    const postParticipantData = debounce(
+        (
+            animateUpdate,
+            contentUpdate,
+            powerModeUpdate,
+            streakUpdate,
+            exclamationUpdate
+        ) => {
+            axios.post("http://localhost:9000/text", {
+                animate: animateUpdate,
+                animationKey,
+                content: contentUpdate,
+                exclamation: exclamationUpdate
+                    ? exclamationUpdate
+                    : exclamation,
+                name,
+                powerMode: powerModeUpdate,
+                streak: streakUpdate
+            });
+        },
+        250
+    );
 
-    const pos = insertTextAction ? data.end : data.start;
-    const token = editor.session.getTokenAt(pos.row, pos.column);
-    if (token) {
-      setInputType(token.type);
-    }
+    const onChange = (value, data) => {
+        const insertTextAction = data.action === "insert";
 
-    setContent(value);
-    clearTimeout(streakTimeout);
-    clearTimeout(saveContentTimeout);
+        const pos = insertTextAction ? data.end : data.start;
+        const token = editor.session.getTokenAt(pos.row, pos.column);
+        if (token) {
+            setInputType(token.type);
+        }
 
-    if (insertTextAction) {
-      updateStreak(streak + 1);
-      setAnimate(true);
-      setAnimationKey(animationKey + 1);
-    }
+        setContent(value);
+        clearTimeout(streakTimeout);
+        clearTimeout(saveContentTimeout);
 
-    streakTimeout = setTimeout(() => {
-      updateStreak(0);
-      setAnimate(false);
-      setPowerMode(false);
-    }, 10000);
+        if (insertTextAction) {
+            updateStreak(streak + 1);
+            setAnimate(true);
+            setAnimationKey(animationKey + 1);
+        }
 
-    saveContentTimeout = setTimeout(() => {
-      localStorage.setItem('content', value);
-    }, 300);
-  };
+        streakTimeout = setTimeout(() => {
+            updateStreak(0);
+            setAnimate(false);
+            setPowerMode(false);
+        }, 10000);
 
-  const getName = () => {
-    const name = window.prompt('What is your name?');
-    setName(name);
-    localStorage.setItem('name', name);
-  };
+        saveContentTimeout = setTimeout(() => {
+            localStorage.setItem("content", value);
+        }, 300);
+    };
 
-  const shake = () => {
-    if (!powerMode) {
-      return;
-    }
+    const getName = () => {
+        const name = window.prompt("What is your name?");
+        setName(name);
+        localStorage.setItem("name", name);
+    };
 
-    const intensity =
-      1 +
-      2 *
-        Math.random() *
-        Math.floor((streak - POWER_MODE_ACTIVATION_THRESHOLD) / 100);
+    const shake = () => {
+        if (!powerMode) {
+            return;
+        }
 
-    const x = intensity * (Math.random() > 0.5 ? -1 : 1);
-    const y = intensity * (Math.random() > 0.5 ? -1 : 1);
+        const intensity =
+            1 +
+            2 *
+                Math.random() *
+                Math.floor((streak - POWER_MODE_ACTIVATION_THRESHOLD) / 100);
 
-    document.getElementById('editor').style.margin = `${y}px ${x}px`;
+        const x = intensity * (Math.random() > 0.5 ? -1 : 1);
+        const y = intensity * (Math.random() > 0.5 ? -1 : 1);
 
-    setTimeout(() => {
-      document.getElementById('editor').style.margin;
-    }, 75);
-  };
+        document.getElementById("editor").style.margin = `${y}px ${x}px`;
 
-  useEffect(() => {
-    let name = localStorage.getItem('name');
-    if (!name) {
-      getName();
-    } else {
-      setName(localStorage.getItem('name'));
-    }
+        setTimeout(() => {
+            document.getElementById("editor").style.margin;
+        }, 75);
+    };
 
-    window.requestAnimationFrame(onFrame);
-  }, []);
+    useEffect(() => {
+        let name = localStorage.getItem("name");
+        if (!name) {
+            getName();
+        } else {
+            setName(localStorage.getItem("name"));
+        }
 
-  useEffect(() => {
-    if (streak > 0 && (streak + 1) % 10 === 0) {
-      setExclamation(sample(EXCLAMATIONS));
-    }
-    if (streak > POWER_MODE_ACTIVATION_THRESHOLD && !powerMode) {
-      setPowerMode(true);
-    }
-    shake();
-    if (editor) {
-      getCursorPosition();
-    }
-    spawnParticles();
-  }, [streak]);
+        window.requestAnimationFrame(onFrame);
+    }, []);
 
-  const onLoad = editor => {
-    setEditor(editor);
-  };
+    useEffect(() => {
+        let tmpExplamation = exclamation;
+        let tmpPowerMode = streak === 0 ? false : powerMode;
+        if (streak > 0 && (streak + 1) % 10 === 0) {
+            const newExclamation = sample(EXCLAMATIONS);
+            setExclamation(newExclamation);
+            tmpExplamation = newExclamation;
+        }
 
-  const getCursorPosition = () => {
-    let { left, top } = editor.renderer.$cursorLayer.getPixelPosition();
-    left += editor.renderer.gutterWidth + 4;
-    top -= editor.renderer.scrollTop;
-    return { x: left, y: top };
-  };
+        if (streak > POWER_MODE_ACTIVATION_THRESHOLD && !powerMode) {
+            setPowerMode(true);
+            tmpPowerMode = true;
+        }
+        shake();
+        if (editor) {
+            getCursorPosition();
+        }
+        spawnParticles();
 
-  const spawnParticles = () => {
-    if (!powerMode) {
-      return;
-    }
-
-    const { x, y } = getCursorPosition();
-    const numParticles = sample(PARTICLE_NUM_RANGE);
-    const color = getParticleColor(inputType);
-    [...Array(numParticles).keys()].forEach(() => {
-      particles[particlePointer] = createParticle(x, y, color);
-      particlePointer = (particlePointer + 1) % MAX_PARTICLES;
-    });
-  };
-
-  const getParticleColor = type => PARTICLE_COLORS[type] || [255, 255, 255];
-
-  const createParticle = (x, y, color) => ({
-    x,
-    y,
-    color,
-    alpha: 1,
-    velocity: {
-      x:
-        PARTICLE_VELOCITY_RANGE.x[0] +
-        Math.random() *
-          (PARTICLE_VELOCITY_RANGE.x[1] - PARTICLE_VELOCITY_RANGE.x[0]),
-
-      y:
-        PARTICLE_VELOCITY_RANGE.y[0] +
-        Math.random() *
-          (PARTICLE_VELOCITY_RANGE.y[1] - PARTICLE_VELOCITY_RANGE.y[0])
-    }
-  });
-
-  const drawParticles = timeDelta => {
-    let canvasContext = ctx;
-    if (!ctx) {
-      const canvas = document.getElementById('canvas');
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      canvasContext = canvas.getContext('2d');
-      setCtx(canvasContext);
-    }
-
-    canvasContext.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    particles.forEach(particle => {
-      if (particle.alpha >= 0.1) {
-        particle.velocity.y += PARTICLE_GRAVITY;
-        particle.x += particle.velocity.x;
-        particle.y += particle.velocity.y;
-        particle.alpha *= PARTICLE_ALPHA_FADEOUT;
-
-        canvasContext.fillStyle = `rgba(${particle.color.join(',')}, ${
-          particle.alpha
-        })`;
-
-        canvasContext.fillRect(
-          Math.round(particle.x - PARTICLE_SIZE / 2),
-          Math.round(particle.y - PARTICLE_SIZE / 2),
-          PARTICLE_SIZE,
-          PARTICLE_SIZE
+        postParticipantData(
+            animate,
+            content,
+            tmpPowerMode,
+            streak,
+            tmpExplamation
         );
-      }
+    }, [streak]);
+
+    const onLoad = editor => {
+        setEditor(editor);
+    };
+
+    const getCursorPosition = () => {
+        let { left, top } = editor.renderer.$cursorLayer.getPixelPosition();
+        left += editor.renderer.gutterWidth + 4;
+        top -= editor.renderer.scrollTop;
+        return { x: left, y: top };
+    };
+
+    const spawnParticles = () => {
+        if (!powerMode) {
+            return;
+        }
+
+        const { x, y } = getCursorPosition();
+        const numParticles = sample(PARTICLE_NUM_RANGE);
+        const color = getParticleColor(inputType);
+        [...Array(numParticles).keys()].forEach(() => {
+            particles[particlePointer] = createParticle(x, y, color);
+            particlePointer = (particlePointer + 1) % MAX_PARTICLES;
+        });
+    };
+
+    const getParticleColor = type => PARTICLE_COLORS[type] || [255, 255, 255];
+
+    const createParticle = (x, y, color) => ({
+        x,
+        y,
+        color,
+        alpha: 1,
+        velocity: {
+            x:
+                PARTICLE_VELOCITY_RANGE.x[0] +
+                Math.random() *
+                    (PARTICLE_VELOCITY_RANGE.x[1] -
+                        PARTICLE_VELOCITY_RANGE.x[0]),
+
+            y:
+                PARTICLE_VELOCITY_RANGE.y[0] +
+                Math.random() *
+                    (PARTICLE_VELOCITY_RANGE.y[1] -
+                        PARTICLE_VELOCITY_RANGE.y[0])
+        }
     });
-  };
 
-  const onFrame = timestamp => {
-    drawParticles(timestamp - lastDraw);
-    setLastDraw(timestamp);
-    window.requestAnimationFrame(onFrame);
-  };
+    const drawParticles = timeDelta => {
+        let canvasContext = ctx;
+        if (!ctx) {
+            const canvas = document.getElementById("canvas");
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            canvasContext = canvas.getContext("2d");
+            setCtx(canvasContext);
+        }
 
-  return (
-    <div className={powerMode ? 'power-mode' : ''}>
-      <>
-        <div className="background" />
-        <canvas id="canvas" />
-        {viewInstructions && (
-          <Instructions closeInstructions={() => setViewInstructions(false)} />
-        )}
-        <AceEditor
-          onLoad={onLoad}
-          mode="html"
-          theme="vibrant_ink"
-          fontSize={20}
-          name="editor"
-          height="100vh"
-          width="100vw"
-          value={content}
-          highlightActiveLine={false}
-          showPrintMargin={false}
-          setOptions={{
-            useWorker: false,
-            showFoldWidgets: false
-          }}
-          session="manual"
-          editorProps={{ $blockScrolling: Infinity }}
-          onChange={onChange}
-        />
+        canvasContext.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        particles.forEach(particle => {
+            if (particle.alpha >= 0.1) {
+                particle.velocity.y += PARTICLE_GRAVITY;
+                particle.x += particle.velocity.x;
+                particle.y += particle.velocity.y;
+                particle.alpha *= PARTICLE_ALPHA_FADEOUT;
 
-        <div className="streak-container">
-          <div className="current">Combo</div>
-          <div key={animationKey} className="counter bump">
-            {streak}
-          </div>
-          <div
-            key={animationKey + 1}
-            className={`bar ${animate && streak !== 0 ? 'animate' : ''}`}
-          />
-          <div className="exclamations">
-            {exclamation && (
-              <span key={exclamation} className="exclamation">
-                {exclamation}
-              </span>
-            )}
-          </div>
+                canvasContext.fillStyle = `rgba(${particle.color.join(",")}, ${
+                    particle.alpha
+                })`;
+
+                canvasContext.fillRect(
+                    Math.round(particle.x - PARTICLE_SIZE / 2),
+                    Math.round(particle.y - PARTICLE_SIZE / 2),
+                    PARTICLE_SIZE,
+                    PARTICLE_SIZE
+                );
+            }
+        });
+    };
+
+    const onFrame = timestamp => {
+        drawParticles(timestamp - lastDraw);
+        setLastDraw(timestamp);
+        window.requestAnimationFrame(onFrame);
+    };
+
+    return (
+        <div className={powerMode ? "power-mode" : ""}>
+            <>
+                <div className="background" />
+                <canvas id="canvas" />
+                {viewInstructions && (
+                    <Instructions
+                        closeInstructions={() => setViewInstructions(false)}
+                    />
+                )}
+                <AceEditor
+                    onLoad={onLoad}
+                    mode="html"
+                    theme="vibrant_ink"
+                    fontSize={20}
+                    name="editor"
+                    height="100vh"
+                    width="100vw"
+                    value={content}
+                    highlightActiveLine={false}
+                    showPrintMargin={false}
+                    setOptions={{
+                        useWorker: false,
+                        showFoldWidgets: false
+                    }}
+                    session="manual"
+                    editorProps={{ $blockScrolling: Infinity }}
+                    onChange={onChange}
+                />
+
+                <div className="streak-container">
+                    <div className="current">Combo</div>
+                    <div key={animationKey} className="counter bump">
+                        {streak}
+                    </div>
+                    <div
+                        key={animationKey + 1}
+                        className={`bar ${
+                            animate && streak !== 0 ? "animate" : ""
+                        }`}
+                    />
+                    <div className="exclamations">
+                        {exclamation && (
+                            <span key={exclamation} className="exclamation">
+                                {exclamation}
+                            </span>
+                        )}
+                    </div>
+                </div>
+
+                <div className="name-tag" onClick={getName}>
+                    {name}
+                </div>
+
+                <div className="power-mode-indicator">
+                    <h1>POWER MODE!</h1>
+                </div>
+
+                <div className="button-bar">
+                    <button className="finish-button">Finish</button>
+                    <button
+                        className="instructions-button"
+                        onClick={() => {
+                            setViewInstructions(true);
+                        }}
+                    >
+                        Instructions
+                    </button>
+                </div>
+            </>
         </div>
-
-        <div className="name-tag" onClick={getName}>
-          {name}
-        </div>
-
-        <div className="power-mode-indicator">
-          <h1>POWER MODE!</h1>
-        </div>
-
-        <div className="button-bar">
-          <button className="finish-button">Finish</button>
-          <button
-            className="instructions-button"
-            onClick={() => {
-              setViewInstructions(true);
-            }}
-          >
-            Instructions
-          </button>
-        </div>
-      </>
-    </div>
-  );
+    );
 };
 
 export default App;
